8036bfd5bf1e87c94766c34be5bf7140a868ca90
blaufish <blaufish@users.noreply.github.com>
2025-08-25 15:00:06 +0200

    Add a command line parser

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,36 @@
 use std::fs;
 use std::io;
 
+use clap::Parser;
+
 mod helpers;
 mod solvers;
 mod sudoku;
 
-fn main() -> io::Result<()> {
-    let file_path = "challenge.txt";
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    file: std::path::PathBuf,
 
-    let contents = fs::read_to_string(file_path)?;
+    #[arg(long, default_value = None)]
+    strategy: Option<String>,
+}
+
+fn main() -> io::Result<()> {
+    let args = Args::parse();
+    let strategy = args.strategy.as_deref();
+
+    let contents = fs::read_to_string(args.file)?;
 
     println!("File contents:\n{}", contents);
 
     let mut sudoku = helpers::parse(contents)?;
 
     helpers::printsudoku(&sudoku);
-    let solved = solvers::solve(&mut sudoku, None);
+
+
+    let solved = solvers::solve(&mut sudoku, strategy);
     println!("Solved: {}", solved);
     helpers::printsudoku(&sudoku);
     Ok(())
