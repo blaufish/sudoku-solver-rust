387bb84b1adbaa7bb5a8b7c59c6b4711ad98f09f
blaufish <blaufish@users.noreply.github.com>
2025-09-03 14:02:20 +0200

    Implement the faster algorithm in solvers.rs too

--- src/solvers.rs
+++ src/solvers.rs
@@ -1,5 +1,8 @@
 use crate::sudoku;
 
+const MAX_GRID_DIMENSIONS : usize = 5;
+const MAX_DIMENSIONS : usize = MAX_GRID_DIMENSIONS * MAX_GRID_DIMENSIONS;
+
 pub fn solve(sudoku: &mut sudoku::Sudoku, strategy: Option<&str>) -> bool {
     let strat;
     match strategy {
@@ -8,7 +11,7 @@ pub fn solve(sudoku: &mut sudoku::Sudoku, strategy: Option<&str>) -> bool {
     }
     match strat {
         "backtrack_faster" => return solve_faster(sudoku),
-        "backtrack_basic" => return solve_basic(sudoku),
+        //"backtrack_basic" => return solve_basic(sudoku),
         _ => {
             println!("Unimplemented: {}", strat);
         }
@@ -17,17 +20,17 @@ pub fn solve(sudoku: &mut sudoku::Sudoku, strategy: Option<&str>) -> bool {
 }
 
 struct Table {
-    rows: [u32; 16],
-    cols: [u32; 16],
-    grids: [[u32; 4]; 4],
+    rows: [u32; MAX_DIMENSIONS],
+    cols: [u32; MAX_DIMENSIONS],
+    grids: [[u32; MAX_GRID_DIMENSIONS]; MAX_GRID_DIMENSIONS],
 }
 
 impl Table {
     fn new() -> Table {
         Table {
-            rows: [0; 16],
-            cols: [0; 16],
-            grids: [[0; 4]; 4],
+            rows: [0; MAX_DIMENSIONS],
+            cols: [0; MAX_DIMENSIONS],
+            grids: [[0; MAX_GRID_DIMENSIONS]; MAX_GRID_DIMENSIONS],
         }
     }
     fn populate(&mut self, sudoku: &sudoku::Sudoku) {
@@ -45,6 +48,48 @@ impl Table {
     }
 }
 
+//Prioritize solving easy squares early, hopefully reducing level of recursion
+fn next_moves(sudoku: &sudoku::Sudoku, table: &Table) -> Option<(usize, usize, Vec<u32>)> {
+    let mut result : Option<(usize, usize, Vec<u32>)> = None;
+    for row in 0..sudoku.dimensions {
+        let utilized_row = table.rows[row];
+        for col in 0..sudoku.dimensions {
+            if sudoku.board[row][col] != 0 {
+                continue;
+            }
+            let utilized_col = table.cols[col];
+            let grid_row = row / sudoku.grid_height;
+            let grid_col = col / sudoku.grid_width;
+            let utilized_grid = table.grids[grid_row][grid_col];
+            let utilized = utilized_row | utilized_col | utilized_grid;
+            let mut moves : Vec<u32> = Vec::new();
+            for i in 0..sudoku.dimensions {
+                let binary: u32 = 1 << i;
+                if binary & utilized != 0 {
+                    continue;
+                }
+                moves.push(binary);
+            }
+            if moves.len() == 0 {
+                //Board is in a bad state, a cell cannot accept any moves
+                return None;
+            }
+            match result {
+                None => result = Some((row, col, moves)),
+                Some((r, c, old_moves)) => {
+                    if moves.len() < old_moves.len() {
+                        result = Some((row, col, moves));
+                    }
+                    else {
+                        result = Some((r, c, old_moves));
+                    }
+                }
+            }
+        }
+    }
+    result
+}
+
 fn solve_faster(sudoku: &mut sudoku::Sudoku) -> bool {
     let mut table = Table::new();
     table.populate(sudoku);
@@ -52,9 +97,9 @@ fn solve_faster(sudoku: &mut sudoku::Sudoku) -> bool {
 }
 
 fn solve_faster_inner(sudoku: &mut sudoku::Sudoku, table: &mut Table) -> bool {
+    let mut row: usize;
+    let mut col: usize;
     let mut solved = true;
-    let mut row: usize = 0;
-    let mut col: usize = 0;
 
     for r in 0..sudoku.dimensions {
         for c in 0..sudoku.dimensions {
@@ -70,18 +115,29 @@ fn solve_faster_inner(sudoku: &mut sudoku::Sudoku, table: &mut Table) -> bool {
     if solved {
         return true;
     }
+
+
+    let moves = next_moves(&sudoku, &table);
+    let row : usize;
+    let col : usize;
+    let values : Vec<u32>;
+    if let Some((r, c, v)) = moves {
+        row = r;
+        col = c;
+        values = v;
+    }
+    else {
+        //Give up. No move is possible.
+        return false;
+    }
+
     let utilized_row = table.rows[row];
     let utilized_col = table.cols[col];
     let grid_row = row / sudoku.grid_height;
     let grid_col = col / sudoku.grid_width;
     let utilized_grid = table.grids[grid_row][grid_col];
     let utilized = utilized_row | utilized_col | utilized_grid;
-    for i in 0..sudoku.dimensions {
-        let binary: u32 = 1 << i;
-        if binary & utilized != 0 {
-            continue;
-        }
-
+    for binary in values {
         sudoku.board[row][col] = binary;
         table.rows[row] ^= binary;
         table.cols[col] ^= binary;
@@ -90,51 +146,12 @@ fn solve_faster_inner(sudoku: &mut sudoku::Sudoku, table: &mut Table) -> bool {
         let recursive_solved = solve_faster_inner(sudoku, table);
         if recursive_solved {
             return true;
-        } else {
-            sudoku.board[row][col] = 0;
-            table.rows[row] ^= binary;
-            table.cols[col] ^= binary;
-            table.grids[grid_row][grid_col] ^= binary;
         }
-    }
-    false
-}
 
-fn solve_basic(sudoku: &mut sudoku::Sudoku) -> bool {
-    let mut solved = true;
-    let mut row: usize = 0;
-    let mut col: usize = 0;
-
-    for r in 0..sudoku.dimensions {
-        for c in 0..sudoku.dimensions {
-            if sudoku.board[r][c] != 0 {
-                continue;
-            }
-            solved = false;
-            row = r;
-            col = c;
-            break;
-        }
-    }
-    if solved {
-        return true;
-    }
-    let utilized_row = sudoku.utilized_row(row);
-    let utilized_col = sudoku.utilized_col(col);
-    let utilized_grid = sudoku.utilized_grid(row, col);
-    let utilized = utilized_row | utilized_col | utilized_grid;
-    for i in 0..sudoku.dimensions {
-        let binary: u32 = 1 << i;
-        if binary & utilized != 0 {
-            continue;
-        }
-        sudoku.board[row][col] = binary;
-        let recursive_solved = solve_basic(sudoku);
-        if recursive_solved {
-            return true;
-        } else {
-            sudoku.board[row][col] = 0;
-        }
+        sudoku.board[row][col] = 0;
+        table.rows[row] ^= binary;
+        table.cols[col] ^= binary;
+        table.grids[grid_row][grid_col] ^= binary;
     }
     false
 }
