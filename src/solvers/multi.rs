9866c37e60366ae38a95c28ac62833faf401da41
blaufish <blaufish@users.noreply.github.com>
2025-09-15 20:38:06 +0200

    Rewrite that appears to remove bug?
    Clarified logic.
    Honestly don't fully get wtf was wrong with original logic

--- src/solvers/multi.rs
+++ src/solvers/multi.rs
@@ -26,7 +26,7 @@ fn solve_inner_inner(sudoku: &mut sudoku::Sudoku, restorepoint: &mut Vec<(usize,
         PreCheckValue::Completed => return true,
         PreCheckValue::NotCompleted => (),
     }
-    let moves = next_moves(&sudoku);
+    let moves = next_moves(sudoku);
     let row: usize;
     let col: usize;
     let values: Vec<u32>;
@@ -49,7 +49,7 @@ fn solve_inner_inner(sudoku: &mut sudoku::Sudoku, restorepoint: &mut Vec<(usize,
 }
 
 //Prioritize solving easy squares early, hopefully reducing level of recursion
-fn next_moves(sudoku: &sudoku::Sudoku) -> Option<(usize, usize, Vec<u32>)> {
+fn next_moves(sudoku: &mut sudoku::Sudoku) -> Option<(usize, usize, Vec<u32>)> {
     let mut table = Table::new();
     table.populate(sudoku);
     let mut result: Option<(usize, usize, Vec<u32>)> = None;
@@ -104,36 +104,43 @@ fn deduce_cell_locked_obvious(
     //println!("deduce_cell_locked_obvious");
     loop {
         let mut done = true;
-        for row in 0..sudoku.dimensions {
-            let utilized_row = table.rows[row];
-            for col in 0..sudoku.dimensions {
-                if sudoku.board[row][col] != 0 {
-                    continue;
-                }
-                let utilized_col = table.cols[col];
-                let grid_row = row / sudoku.grid_height;
-                let grid_col = col / sudoku.grid_width;
-                let utilized_grid = table.grids[grid_row][grid_col];
-                let utilized = utilized_row | utilized_col | utilized_grid;
-                let mut binary: u32 = 0;
-                let mut count = 0;
-                for i in 0..sudoku.dimensions {
-                    let bin: u32 = 1 << i;
-                    if bin & utilized != 0 {
-                        continue;
-                    }
-                    binary = bin;
-                    count = count + 1;
-                }
+        for grid_row in 0..(sudoku.dimensions / sudoku.grid_height) {
+            for grid_col in 0..(sudoku.dimensions / sudoku.grid_width) {
+                let row_base = grid_row * sudoku.grid_height;
+                let col_base = grid_col * sudoku.grid_width;
+                for r in 0..sudoku.grid_height {
+                    for c in 0..sudoku.grid_width {
+                        let row = row_base + r;
+                        let col = col_base + c;
+                        if sudoku.board[row][col] != 0 {
+                            continue;
+                        }
 
-                if count == 1 {
-                    //println!("Fill in! {} {} {}", row, col, binary);
-                    sudoku.board[row][col] = binary;
-                    table.rows[row] ^= binary;
-                    table.cols[col] ^= binary;
-                    table.grids[grid_row][grid_col] ^= binary;
-                    restorepoint.push((row, col));
-                    done = false;
+                        let utilized_grid = table.grids[grid_row][grid_col];
+                        let utilized_row = table.rows[row];
+                        let utilized_col = table.cols[col];
+                        let utilized = utilized_row | utilized_col | utilized_grid;
+                        let mut binary: u32 = 0;
+                        let mut count = 0;
+                        for i in 0..sudoku.dimensions {
+                            let bin: u32 = 1 << i;
+                            if bin & utilized != 0 {
+                                continue;
+                            }
+                            binary = bin;
+                            count = count + 1;
+                        }
+
+                        if count == 1 {
+                            //println!("Fill in! {} {} {}", row, col, binary);
+                            sudoku.board[row][col] = binary;
+                            table.rows[row] ^= binary;
+                            table.cols[col] ^= binary;
+                            table.grids[grid_row][grid_col] ^= binary;
+                            restorepoint.push((row, col));
+                            done = false;
+                        }
+                    }
                 }
             }
         }
