e2fef6bebe983c7aea6372e45fc1f401ed8f0395
blaufish <blaufish@users.noreply.github.com>
2025-09-25 12:36:03 +0200

    Experimental commit: maintain optimization table
    Keep the dynamic table consistent,
    do not recreate it at different times.
    
    Testcase:
       ./target/release/sudoku-solver-rust
       --solve
       --file samples/challenge36x36_2.txt
    
    Time improvment 0.04% negliable, likely within margin of error.
    
    After: 85.562822112s
    Before: 85.597794961s
    
    Also reduces stack frames, less stuff in memory.

--- src/solvers/multi.rs
+++ src/solvers/multi.rs
@@ -2,31 +2,45 @@ use crate::sudoku;
 use crate::table::Table;
 
 pub fn solve(sudoku: &mut sudoku::Sudoku) -> bool {
-    solve_inner(sudoku)
+    let mut table = Table::new();
+    table.populate(sudoku);
+    solve_inner(sudoku, &mut table)
 }
 
-fn restore(sudoku: &mut sudoku::Sudoku, restorepoint: &Vec<(usize, usize)>) {
+fn restore(sudoku: &mut sudoku::Sudoku, table: &mut Table, restorepoint: &Vec<(usize, usize)>) {
     for (row, col) in restorepoint {
+        let binary = sudoku.board[*row][*col];
+        let grid_row = row / sudoku.grid_height;
+        let grid_col = col / sudoku.grid_width;
+
         sudoku.board[*row][*col] = 0;
+
+        table.rows[*row] ^= binary;
+        table.cols[*col] ^= binary;
+        table.grids[grid_row][grid_col] ^= binary;
     }
 }
 
-fn solve_inner(sudoku: &mut sudoku::Sudoku) -> bool {
+fn solve_inner(sudoku: &mut sudoku::Sudoku, table: &mut Table) -> bool {
     let mut restorepoint: Vec<(usize, usize)> = Vec::new();
-    let result = solve_inner_inner(sudoku, &mut restorepoint);
+    let result = solve_inner_inner(sudoku, table, &mut restorepoint);
     if !result {
-        restore(sudoku, &mut restorepoint);
+        restore(sudoku, table, &mut restorepoint);
     }
     result
 }
 
-fn solve_inner_inner(sudoku: &mut sudoku::Sudoku, restorepoint: &mut Vec<(usize, usize)>) -> bool {
-    let check = pre(sudoku, restorepoint);
+fn solve_inner_inner(
+    sudoku: &mut sudoku::Sudoku,
+    table: &mut Table,
+    restorepoint: &mut Vec<(usize, usize)>,
+) -> bool {
+    let check = pre(sudoku, table, restorepoint);
     match check {
         PreCheckValue::Completed => return true,
         PreCheckValue::NotCompleted => (),
     }
-    let moves = next_moves(sudoku);
+    let moves = next_moves(sudoku, table);
     let row: usize;
     let col: usize;
     let values: Vec<u64>;
@@ -39,19 +53,30 @@ fn solve_inner_inner(sudoku: &mut sudoku::Sudoku, restorepoint: &mut Vec<(usize,
         return false;
     }
     for binary in values {
+        let grid_row = row / sudoku.grid_height;
+        let grid_col = col / sudoku.grid_width;
+
         sudoku.board[row][col] = binary;
-        if solve_inner(sudoku) {
+
+        table.rows[row] ^= binary;
+        table.cols[col] ^= binary;
+        table.grids[grid_row][grid_col] ^= binary;
+
+        if solve_inner(sudoku, table) {
             return true;
         }
+
         sudoku.board[row][col] = 0;
+
+        table.rows[row] ^= binary;
+        table.cols[col] ^= binary;
+        table.grids[grid_row][grid_col] ^= binary;
     }
     false
 }
 
 //Prioritize solving easy squares early, hopefully reducing level of recursion
-fn next_moves(sudoku: &sudoku::Sudoku) -> Option<(usize, usize, Vec<u64>)> {
-    let mut table = Table::new();
-    table.populate(sudoku);
+fn next_moves(sudoku: &sudoku::Sudoku, table: &Table) -> Option<(usize, usize, Vec<u64>)> {
     let mut result: Option<(usize, usize, Vec<u64>)> = None;
     for grid_row in 0..(sudoku.dimensions / sudoku.grid_height) {
         for grid_col in 0..(sudoku.dimensions / sudoku.grid_width) {
@@ -172,13 +197,15 @@ fn deduce_completed(sudoku: &sudoku::Sudoku) -> bool {
     true
 }
 
-fn pre(sudoku: &mut sudoku::Sudoku, restorepoint: &mut Vec<(usize, usize)>) -> PreCheckValue {
-    let mut table = Table::new();
-    table.populate(sudoku);
+fn pre(
+    sudoku: &mut sudoku::Sudoku,
+    table: &mut Table,
+    restorepoint: &mut Vec<(usize, usize)>,
+) -> PreCheckValue {
     if deduce_completed(&sudoku) {
         return PreCheckValue::Completed;
     }
-    deduce_cell_locked_obvious(sudoku, &mut table, restorepoint);
+    deduce_cell_locked_obvious(sudoku, table, restorepoint);
     if deduce_completed(&sudoku) {
         return PreCheckValue::Completed;
     }
